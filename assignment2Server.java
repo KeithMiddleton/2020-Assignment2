9bb79623ef57d61c01cadec7270ebad37a369e18
unknown <keith.middleton@ontariotechu.net>
2021-04-02 22:04:04 -0400

    Initial Server Stuff

---
+++ assignment2Server.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.*;
+
+public class assignment2Server {
+	
+	protected Socket clientSocket           = null;
+	protected ServerSocket serverSocket     = null;
+	protected assignment2ServerThread[] threads    = null;
+	protected int numClients                = 0;
+
+	public static int SERVER_PORT = 16789;
+	public static int MAX_CLIENTS = 25;
+
+	public assignment2Server() {
+		try {
+			serverSocket = new ServerSocket(SERVER_PORT);
+			System.out.println("---------------------------");
+			System.out.println("Chat Server Application is running");
+			System.out.println("---------------------------");
+			System.out.println("Listening to port: "+SERVER_PORT);
+			threads = new assignment2ServerThread[MAX_CLIENTS];
+			while(true) {
+				clientSocket = serverSocket.accept();
+				System.out.println("Client #"+(numClients+1)+" connected.");
+				threads[numClients] = new assignment2ServerThread(clientSocket);
+				threads[numClients].start();
+				numClients++;
+			}
+		} catch (IOException e) {
+			System.err.println("IOException while creating server connection");
+		}
+	}
+	
+
+
+	public static void main(String[] args) {
+		assignment2Server newServer = new assignment2Server();
+	}
+}
---
+++ assignment2ServerThread.java
@@ -0,0 +1,78 @@
+import java.io.*; 
+import java.net.*;
+import java.util.*;
+
+public class assignment2ServerThread extends Thread{
+	protected Socket socket       = null;
+	protected PrintWriter out     = null;
+	protected BufferedReader in   = null;
+
+	//Constructor
+	public assignment2ServerThread(Socket socket){
+		super();
+		this.socket = socket;
+		try {
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (IOException e) {
+			System.err.println("IOEXception while opening a read/write connection");
+		}
+	}
+
+
+	public void run(){
+		out.println("Connected");	
+
+		boolean endOfSession = false;
+		while(!endOfSession) {
+			endOfSession = processCommand();
+		}
+		try {
+			socket.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected boolean processCommand(String cmd){
+		if(cmd.equalsIgnoreCase("DIR")){
+
+			File sharedFolder = new File("/shared");
+			File[] files = sharedFolder.listFiles();
+
+			for (File file : files) {
+			    if (file.isFile()) {
+			        out.println(file.getName());
+			    }
+			}
+			return true;
+		}
+		else{
+			out.println("Invalid Command");
+			return true;
+		}
+	}
+
+	protected boolean processCommand(String cmd, String args){
+		if(cmd.equalsIgnoreCase("UPLOAD")){
+			//Temporary
+			out.println("Uploaded.")
+			return true;
+		}
+		else if(cmd.equalsIgnoreCase("DOWNLOAD")){
+			File tempDownload = new File("/shared/" + args);
+			if(tempDownload.exists()){
+				//This is temporary
+				out.println("File exists.")
+			}
+			else{
+				out.println("File does not exist.")
+			}
+			return true;
+		}
+		else{
+			out.println("Invalid Command");
+			return true;
+		}
+	}
+}
